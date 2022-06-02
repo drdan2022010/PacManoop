a30cf72741f586d60a2451d1422f1fbf8701d40e
Trịnh Đức Bảo <78527278+drdan2022010@users.noreply.github.com>
2022-06-02 21:40:20 +0700

    Delete src directory

--- src/StringHelper.java
+++
@@ -1,9 +0,0 @@
-/**
- * Created by Armin on 4/29/2016.
- */
-public class StringHelper {
-    public static int countLines(String str){
-        String[] lines = str.split("\r\n|\r|\n");
-        return  lines.length;
-    }
-}
